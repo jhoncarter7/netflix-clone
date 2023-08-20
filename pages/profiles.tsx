144bec8de8adfcf66dd6f8e3b85e33356d3acccb
jhoncarter7 <rainikhil044@gmail.com>
2023-08-20 20:11:20 +0530

    [feat]: profile route

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,6 +1,8 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -18,7 +20,12 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 const Profiles = () => {
+  const router = useRouter()
     const {data: user} = useCurrentUser();
+    const selectProfile = useCallback(() => {
+      router.push('/');
+    }, [router]);
+  
   return (
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
@@ -26,7 +33,7 @@ const Profiles = () => {
           Who is Watching?
         </h1>
         <div className="flex items-center justify-center gap-8 mt-10">
-          <div onClick={() => {}}>
+          <div onClick={() => selectProfile()}>
             <div className="group flex-row w-44 mx-auto">
               <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
                 <img src="/images/default-blue.png" alt="profile" />
